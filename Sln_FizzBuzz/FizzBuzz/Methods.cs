630e74bac42c3da101274aee4530d310bd41d2c4
Jeferson Echavarria <jfrson24@gmail.com>
2024-01-29 17:46:16 -0500

    The creation of the project is corrected and the Method class is created.

---
+++ Sln_FizzBuzz/FizzBuzz/Methods.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzBuzz
+{
+    public class Methods
+    {
+        #region Constants
+            private readonly int start = 1;
+            private readonly int end = 100;
+            private readonly int zero = 0;
+            private readonly int three = 3;
+            private readonly int five = 5;
+        #endregion
+
+        #region Public methods
+        public void Print()
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i % three == zero) Console.Write("Fizz");
+                else if (i % five == zero) Console.Write("Buzz");
+                else if (i % three == zero && i % five == zero) Console.Write("Fiiz Buzz");
+                else Console.Write(i);
+                Console.WriteLine();
+            }
+        }
+        #endregion
+    }
+}
