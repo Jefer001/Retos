f04cc73468a9277799049fefae407e440e687289
Jeferson Echavarria <jfrson24@gmail.com>
2024-01-30 17:48:39 -0500

    The Fibonacci method is created and with recursion the series is displayed.

---
+++ Sln_Fibonacci/Fibonacci/Methods.cs
@@ -0,0 +1,17 @@
+﻿namespace Fibonacci
+{
+    public class Methods
+    {
+        #region Constants
+        private const int one = 1;
+        #endregion
+
+        #region Public methods
+        public static void Fibonacci(int f, int s, int c, int n)
+        {
+            Console.WriteLine(f + "");
+            if (c < n) Fibonacci (s, f+s, c+one, n);
+        }
+        #endregion
+    }
+}
--- Sln_Fibonacci/Fibonacci/Program.cs
+++ Sln_Fibonacci/Fibonacci/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Fibonacci;
+
+int n = 50;
+Methods.Fibonacci(0, 1, 1, n);
