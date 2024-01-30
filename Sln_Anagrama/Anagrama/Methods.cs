e8caaed862e743cff2bc0f43e95608f882313c32
Jeferson Echavarria <jfrson24@gmail.com>
2024-01-30 16:48:04 -0500

    The static method consists of comparing the two words and knowing if they are anagrams.

--- Sln_Anagrama/Anagrama/Methods.cs
+++ Sln_Anagrama/Anagrama/Methods.cs
@@ -2,6 +2,17 @@
 {
     public class Methods
     {
-
+        #region Public methods
+        public static bool IsAnagrama(string w, string w2)
+        {
+            if (w.Length != w2.Length) return false;
+            else 
+                for (int i = 0; i < w.Length; i++)
+                {
+                    if (w2.Contains(w[1])) return true;
+                }
+            return false;
+        }
+        #endregion
     }
 }
--- Sln_Anagrama/Anagrama/Program.cs
+++ Sln_Anagrama/Anagrama/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Anagrama;
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+Console.WriteLine("Enter the first word");
+string word = Console.ReadLine().Trim();
+Console.WriteLine("Enter the second word");
+string word2 = Console.ReadLine().Trim();
+if (Methods.IsAnagrama(word, word2)) Console.WriteLine("Es Anagrama");
+else Console.WriteLine("No es anagrama");
