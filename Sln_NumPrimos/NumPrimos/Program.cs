087642d23125f638c5fed491d593e9d37f8eb482
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-02 19:12:34 -0500

    An update is made in the program class.

--- Sln_NumPrimos/NumPrimos/Program.cs
+++ Sln_NumPrimos/NumPrimos/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NumPrimos;
 
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
 Methods.Print();
