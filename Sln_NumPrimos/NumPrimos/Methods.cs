0cb1e3432c60bfa7e14381af667778051799aa08
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-02 19:10:13 -0500

    The Method class is created and the Print and Isprime methods are created.

---
+++ Sln_NumPrimos/NumPrimos/Methods.cs
@@ -0,0 +1,37 @@
+﻿
+namespace NumPrimos
+{
+    public class Methods
+    {
+        #region Constants
+        private const int zero = 0;
+        private const int start = 1;
+        private const int two = 2;
+        private const int end = 100;
+        #endregion
+
+        #region Public methods
+        public static void Print()
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i)) {
+                    Console.Write(i);
+                    Console.WriteLine();
+                    }
+                }
+        }
+        #endregion
+
+        #region Private methods
+        private static bool IsPrime(int i)
+        {
+            for (int j = two; j < i; j++)
+            {
+                if (i % j == zero) return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+}
