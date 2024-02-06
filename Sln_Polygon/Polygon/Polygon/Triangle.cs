c36da520f1591e1a033a582aae23ab21c6ec7dcb
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-06 10:51:45 -0500

    The interface is implemented in the Triangle class.

--- Sln_Polygon/Polygon/Polygon/Triangle.cs
+++ Sln_Polygon/Polygon/Polygon/Triangle.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Polygon.Helpers;
 
 namespace Polygon.Polygon
 {
-    public class Triangle
+    public class Triangle : IPolygon
     {
+        #region Constants
+        private const int DOS = 2;
+        #endregion
+
+        #region Attributes
+        private double _height, _base;
+        #endregion
+
+        #region Builder
+        public Triangle(double b, double height)
+        {
+            _base = b;
+            _height = height;
+        }
+        #endregion
+
+        #region Methods public
+        public double Area()
+        {
+            return (_base * _height) / DOS;
+        }
+
+        public string PrintArea()
+        {
+            return $"El área del triángulo es {Area()}";
+        }
+        #endregion
     }
 }
