117c49661b0e47a79d74bb172b8a422ad97d7ec8
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-06 10:31:25 -0500

    The signature of the interface methods is created.

--- Sln_Polygon/Polygon/Helpers/IPolygon.cs
+++ Sln_Polygon/Polygon/Helpers/IPolygon.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Polygon.Helpers
+﻿namespace Polygon.Helpers
 {
     public interface IPolygon
     {
+        double Area();
 
+        string PrintArea();
     }
 }
