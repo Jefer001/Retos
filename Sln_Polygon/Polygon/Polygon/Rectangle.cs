fb53a81b4af1ec6d51018470f5f76f90a40a56b5
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-06 11:04:04 -0500

    The interface is implemented in the Retangle class.

--- Sln_Polygon/Polygon/Polygon/Rectangle.cs
+++ Sln_Polygon/Polygon/Polygon/Rectangle.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Polygon.Helpers;
 
 namespace Polygon.Polygon
 {
-    public class Rectangle
+    public class Rectangle : IPolygon
     {
+        #region Attributes
+        private double _length, _width;
+        #endregion
+
+        #region Builder
+        public Rectangle(double length, double width)
+        {
+            _length = length;
+            _width = width;
+        }
+        #endregion
+
+        #region Methods public
+        public double Area()
+        {
+            return _length * _width;
+        }
+
+        public string PrintArea()
+        {
+            return $"El área del rectángulo es {Area()}";
+        }
+        #endregion
     }
 }
