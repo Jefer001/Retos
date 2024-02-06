5a85440661c05378085e600692c1559baa97970f
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-06 11:10:37 -0500

    The interface is implemented in the Cuadrado class.

--- Sln_Polygon/Polygon/Polygon/Square.cs
+++ Sln_Polygon/Polygon/Polygon/Square.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Polygon.Helpers;
 
 namespace Polygon.Polygon
 {
-    public class Square
+    public class Square : IPolygon
     {
+        #region Constants
+        private const int DOS = 2;
+        #endregion
+
+        #region Attributes
+        private double _side;
+        #endregion
+
+        #region Builder
+        public Square(double side)
+        {
+            _side = side;
+        }
+        #endregion
+
+        #region Methods public
+        public double Area()
+        {
+            return Math.Pow(_side, DOS);
+        }
+
+        public string PrintArea()
+        {
+            return $"El área del cuadrado es {Area()}";
+        }
+        #endregion
     }
 }
