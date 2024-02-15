ffa75227e7c9f98a438c6a08c25d42657ce7f15c
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-15 06:51:18 -0500

    the word counter project is created.

---
+++ Sln_wordCounter/wordCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Crea un programa que cuente cuantas veces se repite cada palabra
+ * y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que
+ *   lo resuelvan automáticamente.
+ */
+
+Console.WriteLine("Hello, World!");
