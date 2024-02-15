5d4119415cc07ae89d72dcb2007ec69c12135978
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-15 06:37:27 -0500

    An update is made to the Program class.

--- Sln_Reverse/Reverse/Program.cs
+++ Sln_Reverse/Reverse/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Reverse;
 
+/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
 Console.WriteLine(Methods.Reverse("This is an example.."));
