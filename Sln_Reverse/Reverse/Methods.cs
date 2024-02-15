7510beda765af4059a09ee479de72a6375aa4858
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-15 06:29:17 -0500

    The Method class and the Inverse method are created.

---
+++ Sln_Reverse/Reverse/Methods.cs
@@ -0,0 +1,16 @@
+﻿namespace Reverse
+{
+    public class Methods
+    {
+        public static string Reverse(string str)
+        {
+            Char[] chars = str.ToCharArray();
+            string res = string.Empty;
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                res += chars[i];
+            }
+            return res;
+        }
+    }
+}
