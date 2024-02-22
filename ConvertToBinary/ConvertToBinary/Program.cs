a77d5b25e6be04d4cdfc7d8fb21b403f2b2fb779
Jeferson Echavarria <jfrson24@gmail.com>
2024-02-22 06:26:54 -0500

    The ConvertToBinary project is created.

---
+++ ConvertToBinary/ConvertToBinary/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Crea un programa se encargue de transformar un número
+ * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ */
+
+Console.WriteLine("Hello, World!");
